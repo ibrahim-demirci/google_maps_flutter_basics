63154e7d344cd78cccace87335a5306e32c4200c
ibrahim-demirci <ibrahim.demirci@marun.edu.tr>
2021-04-03 00:14:16 +0300

    upload project

---
+++ README.md
@@ -0,0 +1,44 @@
+
+# Google Maps Marker And Camera Control in Flutter  
+  
+Add markers and change camera position in Google Maps.   
+
+## Screenshots
+
+![image](screenshots/image1.jpeg)
+![image](screenshots/image2.jpeg)
+
+  
+## Implementation  
+  
+> Add dependencies to pubspec.yaml file.  
+  
+<pre><code>google_maps_flutter: ^0.5.24+1</code></pre>  
+>How to get API Key :  https://developers.google.com/maps/documentation/android-sdk/get-api-key  
+> You have to get API key (I will explain soon) and add this code   
+
+```xml  
+<meta-data android:name="com.google.android.geo.API_KEY"  
+                                   android:value="YOUR KEY HERE" /> 
+```                      
+> in **project/android/app/src/main/AndroidManifest.xml** file like the code below.  <br>
+>How to get API Key :  https://developers.google.com/maps/documentation/android-sdk/get-api-key  
+  
+```xml
+ <application
+                   android:name="io.flutter.app.FlutterApplication"
+                   android:label="flutter_maps_basics"
+                   android:icon="@mipmap/ic_launcher">
+           
+                   <meta-data android:name="com.google.android.geo.API_KEY"
+                       android:value="YOUR KEY HERE" />
+                   <activity
+                       android:name=".MainActivity"
+                       android:launchMode="singleTop"
+                       android:theme="@style/LaunchTheme"
+                       android:configChanges="orientation|keyboardHidden|keyboard|screenSize|smallestScreenSize|locale|layoutDirection|fontScale|screenLayout|density|uiMode"
+                       android:hardwareAccelerated="true"
+                       android:windowSoftInputMode="adjustResize">
+                       
+```
+
---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // controller  for move the map in application
+  GoogleMapController _controller;
+
+  // this value is first  position when map is start.
+  final CameraPosition _initialPosition =
+      CameraPosition(target: LatLng(41.017901, 28.847953));
+
+  // Marker list for  places to be marked when clicking on the map
+  final List<Marker> markers = [];
+
+  addMarker(cordinate) {
+    int id = Random().nextInt(100);
+
+    setState(() {
+      markers
+          .add(Marker(position: cordinate, markerId: MarkerId(id.toString())));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: GoogleMap(
+          initialCameraPosition: _initialPosition,
+          mapType: MapType.normal,
+          onMapCreated: (controller) {
+            setState(() {
+              _controller = controller;
+            });
+          },
+          markers: markers.toSet(),
+
+          //the clicked position will be centered and marked
+          onTap: (cordinate) {
+            _controller.animateCamera(CameraUpdate.newLatLng(cordinate));
+            addMarker(cordinate);
+          },
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            _controller.animateCamera(CameraUpdate.zoomOut());
+          },
+          child: Icon(Icons.zoom_out),
+        ));
+  }
+}
